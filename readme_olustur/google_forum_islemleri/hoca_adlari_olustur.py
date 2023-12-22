cf87a06be521d5fdf001702eb2f32aa889a541d1
muhammed kayra <kayrabulut39@gmail.com>
2023-12-22 15:30:37 +0300

    google forum soruları sıralı hale getirildi

--- readme_olustur/google_forum_islemleri/hoca_adlari_olustur.py
+++ readme_olustur/google_forum_islemleri/hoca_adlari_olustur.py
@@ -12,10 +12,35 @@ json_file_path = '../hocalar.json'  # JSON dosyasının yolu
 # JSON dosyasını oku
 with open(json_file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
+def sirala_ve_ayir(hocalar_listesi):
+    """
+    Hocaların adlarını, ünvanlarına göre önceliklendirilmiş bir sırayla sıralar.
+    Önce 'Prof. Dr.', sonra 'Doç. Dr.', ardından 'Dr.' ve son olarak ünvanı olmayanları sıralar.
+    """
+    # Unvanlara öncelik değerleri atayalım.
+    unvanlar = {
+        "Prof. Dr.": 1,
+        "Prof.": 1,
+        "Doç. Dr.": 2,
+        "Doç.": 2,
+        "Dr.": 3
+    }
 
-# Hocaların adlarını bir liste olarak al
+    def unvan_önceligi(hoca_adi):
+        # Hocanın adındaki unvanı bul ve öncelik değerini döndür
+        for unvan, öncelik in unvanlar.items():
+            if hoca_adi.startswith(unvan):
+                return öncelik
+        # Eğer hiçbir unvan eşleşmiyorsa, varsayılan olarak en düşük önceliği (en yüksek sayıyı) ver
+        return max(unvanlar.values()) + 1
+
+    # Hocaların adlarını önceliklere göre ve alfabetik sıraya göre sırala
+    sirali_hocalar = sorted(hocalar_listesi, key=lambda hoca: (unvan_önceligi(hoca), hoca))
+    return sirali_hocalar
+
+# Hoca adlarını işle ve unvanları at
 hoca_names = [hoca['ad'] for hoca in data['hocalar']]
-
+hoca_names = sirala_ve_ayir(hoca_names)
 # CSV dosyası oluştur
 csv_file_path = 'hoca_listesi.csv'  # Oluşturulacak CSV dosyasının yolu
 with open(csv_file_path, 'w', newline='', encoding='utf-8') as file:
