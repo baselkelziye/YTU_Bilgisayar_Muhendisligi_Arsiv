ac47336798d4af4f76d77fb7bbf14472a0f1271a
muhammed kayra <kayrabulut39@gmail.com>
2023-12-22 15:30:37 +0300

    google forum soruları sıralı hale getirildi

--- readme_olustur/google_forum_islemleri/ders_adlari_olustur.py
+++ readme_olustur/google_forum_islemleri/ders_adlari_olustur.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import json
+import locale
 
+# Türkçe karakterlerin doğru sıralanabilmesi için Türkçe locale ayarını kullanacağız.
+# Sistem locale ayarlarınızın bu dili desteklediğinden emin olun.
+locale.setlocale(locale.LC_ALL, 'tr_TR.utf8')
+
+def turkce_sirala(ders_listesi):
+    """
+    Verilen ders adlarını Türkçe karakterleri de dikkate alarak alfabetik sıraya dizer.
+    """
+    return sorted(ders_listesi, key=locale.strxfrm)
 # JSON dosyasını oku
 dosya_yolu = '../dersler.json'
 with open(dosya_yolu, 'r', encoding='utf-8') as dosya:
@@ -8,6 +18,7 @@ with open(dosya_yolu, 'r', encoding='utf-8') as dosya:
 
 # Ders adlarını içeren bir liste oluştur
 ders_adlari = [ders['ad'] for ders in veri['dersler']]
+ders_adlari = turkce_sirala(ders_adlari)
 
 # Ders adlarını bir DataFrame'e dönüştür
 df = pd.DataFrame(ders_adlari, columns=['Ders Adı'])
