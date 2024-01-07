3e8546aa534556510f597904fe485946cae5231b
kaayra2000 <kayrabulut39@gmail.com>
2024-01-07 13:04:59 +0300

    değişiklikler senkronize edildi

--- readme_olustur/degisiklikleri_githuba_yolla.bat
+++ readme_olustur/degisiklikleri_githuba_yolla.bat
@@ -7,7 +7,9 @@ IF NOT %ERRORLEVEL% == 0 (
     echo Bu dizin bir Git reposu değil.
     exit /b 1
 )
-
+git config --global i18n.commitEncoding utf-8
+git config --global i18n.logOutputEncoding utf-8
+CHCP 65001
 REM Değişiklikleri ekler, commit yapar ve push yapar.
 git add --all
 git commit -m "değişiklikler senkronize edildi"
--- readme_olustur/degisiklikleri_githuba_yolla.sh
+++ readme_olustur/degisiklikleri_githuba_yolla.sh
@@ -7,7 +7,10 @@ if [ $? -ne 0 ]; then
     echo "Bu dizin bir Git reposu değil."
     exit 1
 fi
+git config --global i18n.commitEncoding utf-8
+git config --global i18n.logOutputEncoding utf-8
 
+export LANG=en_US.UTF-8
 # Değişiklikleri ekler, commit yapar ve push yapar.
 git add --all
 git commit -m "değişiklikler senkronize edildi"
