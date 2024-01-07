70258b83089a35db70c5bd191f97897d765c0920
kaayra2000 <kayrabulut39@gmail.com>
2024-01-07 13:03:17 +0300

    de─şi┼şiklikler senkronize edildi

---
+++ readme_olustur/degisiklikleri_githubdan_cek.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Bu script, uzak Git deposundaki değişiklikleri yerel depoya çeker ve birleştirir.
+
+REM Mevcut dizinin bir Git reposu olup olmadığını kontrol eder.
+git rev-parse --is-inside-work-tree >nul 2>&1
+IF NOT %ERRORLEVEL% == 0 (
+    echo Bu dizin bir Git reposu değil.
+    exit /b 1
+)
+
+REM Uzak depodaki değişiklikleri çeker ve yerel depoya birleştirir.
+git fetch --all
+git reset --hard origin/master
