9311ff1a1b2d2665ec237268db1b7a6f850a7c76
kaayra2000 <kayrabulut39@gmail.com>
2024-01-23 21:09:42 +0300

    arayüz güncellendi

--- readme_olustur/google_forum_islemleri/google_form_rutin_kontrol.py
+++ readme_olustur/google_forum_islemleri/google_form_rutin_kontrol.py
@@ -3,7 +3,6 @@ import hashlib
 import os
 import subprocess
 import time
-import json
 import sys
 
 sys.path.append(
@@ -22,7 +21,7 @@ def check_for_updates(key, url):
 
     # Eğer bu URL daha önce kontrol edildiyse ve hash değeri değişmişse, güncelleme olduğunu bildir
     if url in previous_hashes and previous_hashes[url] != current_hash:
-        print(f"Değişiklik bulundu: {key}")
+        sys.stdout.write(f"Değişiklik bulundu: {key}\n")
         previous_hashes[url] = current_hash
         return True
 
@@ -32,7 +31,7 @@ def check_for_updates(key, url):
 
 
 def execute_command(command):
-    print(f"Komut çalıştırılıyor: {command}")
+    sys.stdout.write(f"Komut çalıştırılıyor: {command}\n")
     try:
         # Komutu çalıştır ve çıktıyı yakala
         result = subprocess.run(
@@ -43,10 +42,10 @@ def execute_command(command):
             stderr=subprocess.PIPE,
         )
         # Komut başarıyla çalıştıysa, çıktıyı yazdır
-        print(result.stdout.decode())
+        sys.stdout.write(result.stdout.decode()+ "\n")
     except subprocess.CalledProcessError as e:
         # Komut hata ile sonuçlanırsa, hatayı yazdır ve script'i durdur
-        print(f"Komut hatası: {e.stderr.decode()}")
+        sys.stderr.write(f"Komut hatası: {e.stderr.decode()}\n")
         return False
     return True
 
@@ -54,15 +53,10 @@ def execute_command(command):
 def update_repository():
     # Mevcut çalışma dizinini sakla
     original_directory = os.getcwd()
-    print("Güncellemeler uygulanıyor...")
-    readme_guncelle_sc = (
-        README_GUNCELLE_BAT if sys.platform.startswith("win") else README_GUNCELLE_SH
-    )
-    google_form_guncelle_sc = (
-        GOOGLE_FORM_GUNCELLE_BAT
-        if sys.platform.startswith("win")
-        else GOOGLE_FORM_GUNCELLE_SH
-    )
+    sys.stdout.write("Güncellemeler uygulanıyor...\n")
+    readme_guncelle_komutu = f"python3 readme_olustur.py"
+    # google form güncelle komutu
+    google_form_guncelle_komutu = f"python3 hoca_icerikleri_guncelle.py && python3 ders_icerikleri_guncelle.py"
     # Git ve Python komutlarını sırayla çalıştır
     try:
         repo_yolu = os.path.join(BIR_UST_DIZIN, DOKUMANLAR_REPO_YOLU)
@@ -70,41 +64,41 @@ def update_repository():
         stream = os.popen("git status")
         output = stream.read()
         if "nothing to commit, working tree clean" not in output:
-            print(
+            sys.stderr.write(
                 "Dizinde değişiklikler var. Lütfen önce bu değişiklikleri commit yapın veya geri alın. Script durduruluyor."
             )
-            time.sleep(10)
             exit(1)
         if not execute_command("git fetch"):
-            print("Fetch sırasında conflict oluştu, script durduruluyor.")
+            sys.stderr.write("Fetch sırasında conflict oluştu, script durduruluyor.\n")
             return
         if not execute_command("git reset --hard origin/main"):
-            print("Reset sırasında conflict oluştu, script durduruluyor.")
+            sys.stderr.write("Reset sırasında conflict oluştu, script durduruluyor.\n")
             return
         if not execute_command("git pull"):
-            print("Pull sırasında conflict oluştu, script durduruluyor.")
+            sys.stderr.write("Pull sırasında conflict oluştu, script durduruluyor.\n")
             return
-        os.chdir(os.path.join(original_directory, BIR_UST_DIZIN))
-        if not execute_command(google_form_guncelle_sc + " bosa_bekleme"):
-            print(
+        os.chdir(original_directory)
+        if not execute_command(google_form_guncelle_komutu):
+            sys.stderr.write(
                 "Google form içerikleri güncellenirken hata oluştu, script durduruluyor."
             )
             return
-        os.system(readme_guncelle_sc + " bosa_bekleme")
+        os.chdir(BIR_UST_DIZIN)
+        os.system(readme_guncelle_komutu)
         os.chdir(DOKUMANLAR_REPO_YOLU)
         if not execute_command("git add --all"):
-            print("Git add sırasında conflict oluştu, script durduruluyor.")
+            sys.stderr.write("Git add sırasında conflict oluştu, script durduruluyor.\n")
             return
         if not execute_command('git commit -m "rutin readme güncellemesi (robot)"'):
-            print("Git commit sırasında conflict oluştu, script durduruluyor.")
+            sys.stderr.write("Git commit sırasında conflict oluştu, script durduruluyor.\n")
             return
         if not execute_command("git push"):
-            print("Git push sırasında conflict oluştu, script durduruluyor.")
+            sys.stderr.write("Git push sırasında conflict oluştu, script durduruluyor.\n")
             return
     except Exception as e:
         # Hata oluşursa, hatayı yazdır ve e-posta gönder
         error_message = f"Script hatası: {e}"
-        print(error_message)
+        sys.stderr.write(error_message)
     finally:
         # Başlangıç dizinine geri dön, hata olsa bile
         os.chdir(original_directory)
@@ -127,19 +121,19 @@ update_repository()
 i = 0
 timeout = 180
 div = 3
-print("Script çalışıyor...")
+sys.stdout.write("Script çalışıyor...\n")
 # Sonsuz döngü içinde URL'leri kontrol et ve güncelle
 while True:
     for key, url in urls.items():
         if check_for_updates(key, url):
-            print(f"Güncelleme tespit edildi: {key}")
+            sys.stdout.write(f"Güncelleme tespit edildi: {key}\n")
             update_repository()
         else:
-            print(f"Güncelleme yok: {key}")
+            sys.stdout.write(f"Güncelleme yok: {key}\n")
     i += 1
-    print(f"{i}. kontrol tamamlandı.")
+    sys.stdout.write(f"{i}. kontrol tamamlandı.\n")
     for k in range(0, int(timeout / div)):
-        print(
-            f"{timeout-k*div} saniye sonra kontrol edilecek.", end="\r"
+        sys.stdout.write(
+            f"{timeout-k*div} saniye sonra kontrol edilecek.\n"
         )  # '\r' ile satırın başına dön
         time.sleep(div)
--- readme_olustur/readme_olustur.py
+++ readme_olustur/readme_olustur.py
@@ -134,17 +134,17 @@ def hocalari_readme_ye_ekle(bilgiler):
     if HOCALAR not in bilgiler or len(bilgiler[HOCALAR]) != 0:
         bilgiler[HOCALAR] = [hoca for hoca in bilgiler[HOCALAR] if hoca[AD] != ""]
     else:
-        print("Hoca bilgileri bulunamadı.")
+        sys.stderr.write("Hoca bilgileri bulunamadı.\n")
         return
     with open(ANA_README_YOLU, "a", encoding="utf-8") as f:
         if BOLUM_ADI in bilgiler:
             f.write(f"\n\n\n## 🎓 {bilgiler[BOLUM_ADI]}\n")
         else:
-            print("Hocalar Bölüm adı bulunamadı.")
+            sys.stderr.write("Hocalar Bölüm adı bulunamadı.\n")
         if BOLUM_ACIKLAMASI in bilgiler:
             f.write(f"📚 {bilgiler[BOLUM_ACIKLAMASI]}\n\n\n\n")
         else:
-            print("Hocalar Bölüm açıklaması bulunamadı.")
+            sys.stderr.write("Hocalar Bölüm açıklaması bulunamadı.\n\n")
         en_populer_hoca_oy_sayisi = 0
         en_populer_hoca_adi = ""
         if EN_POPULER_HOCA in bilgiler and HOCA_ADI in bilgiler[EN_POPULER_HOCA]:
@@ -153,9 +153,9 @@ def hocalari_readme_ye_ekle(bilgiler):
                 en_populer_hoca_oy_sayisi = bilgiler[EN_POPULER_HOCA][OY_SAYISI]
             else:
                 en_populer_hoca_oy_sayisi = 1
-                print("En popüler hoca oy sayısı bulunamadı.")
+                sys.stderr.write("En popüler hoca oy sayısı bulunamadı.\n")
         else:
-            print("En popüler hoca bilgileri bulunamadı.")
+            sys.stderr.write("En popüler hoca bilgileri bulunamadı.\n")
 
         for hoca in sorted(bilgiler[HOCALAR], key=hoca_siralama_anahtari):
             if AD not in hoca:
@@ -282,7 +282,7 @@ def dersleri_readme_ye_ekle(dersler):
         elif TIP in ders:
             donem_key = ders[TIP]
         else:
-            print("Ders bilgileri bulunamadı.")
+            sys.stderr.write("Ders bilgileri bulunamadı.\n")
             continue
         if donem_key not in gruplanmis_dersler:
             gruplanmis_dersler[donem_key] = []
@@ -441,14 +441,12 @@ def readme_ye_yazar_notlari_ekle(yazar_notlari):
 
 def readme_katkida_bulunanlar_ekle(veri):
     with open(ANA_README_YOLU, "a", encoding="utf-8") as f:
-        f.write(
-            f"\n\n## 🤝 {veri['bolum_adi']}\n\n"
-        )  # El sıkışma emoji, işbirliğini ve katkıyı temsil eder
+        f.write(f"\n\n## 🤝 {veri['bolum_adi']}\n\n")
         f.write(f"{veri['bolum_aciklamasi']}\n\n")
         for katkida_bulunan in veri["katkida_bulunanlar"]:
             f.write(
-                f"- 👤 {katkida_bulunan[AD]}\n  - 🔗 Github Adresi: {katkida_bulunan['github_link']}\n"
-            )  # Kişi ve link emojisi
+                f"- ✨👤✨ {katkida_bulunan[AD]} ✨\n  - 🔗 [Github Profili]({katkida_bulunan['github_link']})\n"
+            )
 
 
 def readmeye_star_history_ekle():
@@ -478,7 +476,7 @@ def sıralama_anahtarı(ders):
 """
 BURASI ANA README OLUŞTURMA KISMI
 """
-print("README.md oluşturuluyor...")
+sys.stdout.write("README.md oluşturuluyor...\n")
 # JSON dosyasından yazar notlarını oku ve README'ye ekle
 yazar_notlari = json_oku(YAZARIN_NOTLARI_JSON_NAME)
 # JSON dosyasından repo kullanımı bilgilerini oku ve README'ye ekle
@@ -491,40 +489,40 @@ hocalar = json_oku(HOCALAR_JSON_NAME)
 giris_bilgileri = json_oku(GIRIS_JSON_NAME)
 katkida_bulunanlar = json_oku(KATKIDA_BULUNANLAR_JSON_NAME)
 if giris_bilgileri is not None:
-    print("Giriş bilgileri README'ye ekleniyor...")
+    sys.stdout.write("Giriş bilgileri README'ye ekleniyor...\n")
     readme_ye_giris_ekle(giris_bilgileri)
 else:
-    print("Giriş bilgileri bulunamadı...")
+    sys.stdout.write("Giriş bilgileri bulunamadı...\n")
 if repo_kullanimi_bilgileri is not None:
-    print("Repo kullanımı README'ye ekleniyor...")
+    sys.stdout.write("Repo kullanımı README'ye ekleniyor...\n")
     readme_ye_repo_kullanimi_ekle(repo_kullanimi_bilgileri)
 else:
-    print("Repo kullanımı bilgileri bulunamadı...")
+    sys.stderr.write("Repo kullanımı bilgileri bulunamadı...\n")
 if dersler is not None:
-    print("Ders bilgileri README'ye ekleniyor...")
+    sys.stdout.write("Ders bilgileri README'ye ekleniyor...\n")
     dersleri_readme_ye_ekle(dersler)
 if hocalar is not None:
-    print("Hoca bilgileri README'ye ekleniyor...")
+    sys.stdout.write("Hoca bilgileri README'ye ekleniyor...\n")
     hocalari_readme_ye_ekle(hocalar)
 else:
-    print("Hoca bilgileri bulunamadı...")
+    sys.stderr.write("Hoca bilgileri bulunamadı...\n")
 if yazar_notlari is not None:
-    print("Yazar notları README'ye ekleniyor...")
+    sys.stdout.write("Yazar notları README'ye ekleniyor...\n")
     readme_ye_yazar_notlari_ekle(yazar_notlari)
 else:
-    print("Yazar notları bulunamadı...")
+    sys.stderr.write("Yazar notları bulunamadı...\n")
 if hocalar is not None:
-    print("Hoca kısaltmaları README'ye ekleniyor...")
+    sys.stdout.write("Hoca kısaltmaları README'ye ekleniyor...\n")
     readmeye_hocalar_icin_kisaltmalar_ekle(hocalar)
 else:
-    print("Hoca kısaltmaları bulunamadı...")
+    sys.stderr.write("Hoca kısaltmaları bulunamadı...\n")
+sys.stdout.write("Star history README'ye ekleniyor...\n")
+readmeye_star_history_ekle()
 if katkida_bulunanlar is not None:
-    print("Katkıda bulunanlar README'ye ekleniyor...")
+    sys.stdout.write("Katkıda bulunanlar README'ye ekleniyor...\n")
     readme_katkida_bulunanlar_ekle(katkida_bulunanlar)
 else:
-    print("Katkıda bulunanlar bulunamadı...")
-print("Star history README'ye ekleniyor...")
-readmeye_star_history_ekle()
+    sys.stderr.write("Katkıda bulunanlar bulunamadı...\n")
 """
 BURASI ANA README OLUŞTURMA KISMI
 """
@@ -620,9 +618,9 @@ def klasorde_baska_dosya_var_mi(ders_klasoru):
 
 
 if dersler is not None:
-    print("Dersler README.md oluşturuluyor...")
+    sys.stdout.write("Dersler README.md oluşturuluyor...\n")
     for ders in dersler[DERSLER]:
-        print(f"{ders[AD]} README.md oluşturuluyor...")
+        sys.stdout.write(f"{ders[AD]} README.md oluşturuluyor...\n")
         ders_klasoru = en_iyi_eslesen_klasor_yolu_bul(DOKUMANLAR_REPO_YOLU, ders[AD])
         if ders_klasoru is not None:
             baska_dosya_var_mi = klasorde_baska_dosya_var_mi(ders_klasoru)
@@ -639,9 +637,9 @@ if dersler is not None:
             ders_klasorune_readme_olustur(
                 ders, ders_klasoru, klasor_sonradan_olustu=True
             )
-        print(f"{ders[AD]} README.md oluşturuldu.")
+        sys.stdout.write(f"{ders[AD]} README.md oluşturuldu.\n")
 else:
-    print("Ders bilgileri bulunamadı.")
+    sys.stderr.write("Ders bilgileri bulunamadı.\n")
 """
 BURASI DERSLER README OLUŞTURMA KISMI
 """
@@ -655,7 +653,7 @@ Burası Dönem Readme oluşturma kısmı
 def donemlere_gore_readme_olustur(donemler):
     # Her dönem için README.md oluştur
     for donem in donemler[DONEMLER]:
-        print(f"{donem[DONEM_ADI]} README.md oluşturuluyor...")
+        sys.stdout.write(f"{donem[DONEM_ADI]} README.md oluşturuluyor...\n")
         donem_dosya_yolu = donem_dosya_yolu_getir(donem, DOKUMANLAR_REPO_YOLU)
         os.makedirs(donem_dosya_yolu, exist_ok=True)
         dosya_yolu = os.path.join(donem_dosya_yolu, README_MD)
@@ -670,7 +668,7 @@ def donemlere_gore_readme_olustur(donemler):
             if donem[DONEM_ADI] != "Mesleki Seçmeli Dersler":
                 f.write("## 📚 Dönemin Zorunlu Dersleri\n\n")
                 # Kitap emoji, zorunlu dersleri temsil eder
-        print(f"{donem[DONEM_ADI]} README.md oluşturuldu.")
+        sys.stdout.write(f"{donem[DONEM_ADI]} README.md oluşturuldu.\n")
 
 
 def ders_bilgilerini_readme_ile_birlestir(
@@ -678,7 +676,7 @@ def ders_bilgilerini_readme_ile_birlestir(
 ):
     # Her ders için ilgili dönem README'sine ekle
     for ders in dersler:
-        print(f"{ders[AD]} README.md dönemine ekleniyor...")
+        sys.stdout.write(f"{ders[AD]} README.md dönemine ekleniyor...\n")
         for donem in donemler:
             if ders[YIL] == donem[YIL] and ders[DONEM] == donem[DONEM]:
                 dosya_yolu = os.path.join(
@@ -753,17 +751,17 @@ def ders_bilgilerini_readme_ile_birlestir(
                     if GUNCEL_MI in ders and not ders[GUNCEL_MI]:
                         f.write("\n#### ℹ️ Dersin içeriği güncel değil\n")
                         f.write(f"- {guncel_olmayan_ders_aciklamasi}\n")
-        print(f"{ders[AD]} README.md dönemine eklendi.")
+        sys.stdout.write(f"{ders[AD]} README.md dönemine eklendi.\n")
 
 
 donemler = json_oku(DONEMLER_JSON_NAME)
 if donemler is not None:
-    print("Dönem README'leri oluşturuluyor...")
-    print("Dönem bilgileri README'ye ekleniyor...")
+    sys.stdout.write("Dönem README'leri oluşturuluyor...\n")
+    sys.stdout.write("Dönem bilgileri README'ye ekleniyor...\n")
     donemlere_gore_readme_olustur(donemler)
-    print("Ders bilgileri README'ye ekleniyor...")
+    sys.stdout.write("Ders bilgileri README'ye ekleniyor...\n")
     ders_bilgilerini_readme_ile_birlestir(
         dersler[DERSLER], donemler[DONEMLER], dersler[GUNCEL_OLMAYAN_DERS_ACIKLAMASI]
     )
 else:
-    print("Dönem bilgileri bulunamadı.")
+    sys.stderr.write("Dönem bilgileri bulunamadı.\n")
