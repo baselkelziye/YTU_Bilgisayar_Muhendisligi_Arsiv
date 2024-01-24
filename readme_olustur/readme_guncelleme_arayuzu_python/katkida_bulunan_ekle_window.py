cab75f12f17f0951ccef2407a0a82ee598847133
muhammed kayra <kayrabulut39@gmail.com>
2024-01-24 16:26:42 +0300

    arayüz kodları güncellendi

--- readme_olustur/readme_guncelleme_arayuzu_python/katkida_bulunan_ekle_window.py
+++ readme_olustur/readme_guncelleme_arayuzu_python/katkida_bulunan_ekle_window.py
@@ -3,82 +3,179 @@ from PyQt5.QtWidgets import (
     QDialog,
     QPushButton,
     QVBoxLayout,
-    QDesktopWidget,
     QLabel,
     QMessageBox,
     QComboBox,
+    QWidget,
+    QScrollArea,
+    QHBoxLayout
 )
+import re
 from threadler import KatkiEkleThread
 from progress_dialog import CustomProgressDialog
 from degiskenler import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from close_event import closeEventHandler
-
-
-class KatkidaBulunanEkleWindow(QDialog):
+from metin_islemleri import kisaltMetin
+class BaseKatkidaBulunanWindow(QDialog):
     def __init__(self, parent):
-        super().__init__()
+        super().__init__(parent)
         self.setModal(True)
+        self.iletisim_bilgisi_sayisi=0
+        self.iletisim_bilgileri_dizisi = {}
         self.is_programmatic_close = False
-        self.parent = parent
-        self.title = "Katkıda Bulunan Ekle"
         self.initUI()
         if os.path.exists(SELCUKLU_ICO_PATH):
             self.setWindowIcon(QIcon(SELCUKLU_ICO_PATH))
 
     def initUI(self):
-        self.setWindowTitle(self.title)
         self.setMinimumSize(
-            700, 200
+            600, 400
         )  # Pencerenin en küçük olabileceği boyutu ayarlayın
-
-        layout = QVBoxLayout()
+        self.layout = QVBoxLayout()
         self.progressDialog = CustomProgressDialog("Kontrol Ediliyor...", self)
         self.progressDialog.close()
         # Ad ve GitHub Linki için giriş alanları
-        self.name_label = QLabel("Ad")
-        self.name_label.setAlignment(Qt.AlignCenter)
-        self.name_input = QLineEdit()
-        self.github_label = QLabel("GitHub Kullanıcı Adı")
-        self.github_label.setAlignment(Qt.AlignCenter)
-        self.github_input = QLineEdit()
+        self.ad_label = QLabel("Ad")
+        self.ad_label.setAlignment(Qt.AlignCenter)
+        self.ad_input = QLineEdit()
         self.katkida_bulunma_orani_label = QLabel("Katkıda Bulunma Oranı")
         self.katkida_bulunma_orani_label.setAlignment(Qt.AlignCenter)
         self.katkida_bulunma_orani = QComboBox()
         self.katkida_bulunma_orani.addItems(KATKIDA_BULUNMA_ORANI_DIZI)
+        self.katkida_bulunma_orani.setCurrentText(KATKIDA_BULUNMA_ORANI_DIZI[-1])
+        # Not ekleme butonu
+        self.iletisimBilgisiEkleBtn = QPushButton("İletişim Bilgisi Ekle", self)
+        self.iletisimBilgisiEkleBtn.setStyleSheet(EKLE_BUTONU_STILI)  # Yeşil arka plan, beyaz yazı
+        self.iletisimBilgisiEkleBtn.clicked.connect(lambda: self.iletisimBilgisiEkle("",""))
+        # Not sayısını gösteren etiket
+        self.iletisimBilgisiLabel = QLabel("Toplam 0 iletişim bilgisi")
+        self.iletisimBilgisiLabel.setAlignment(Qt.AlignCenter)
+
+        # Kaydırılabilir alan oluştur
+        self.scrollArea = QScrollArea(self)  # ScrollArea oluştur
+        self.scrollArea.setWidgetResizable(True)
+
+        # Linkleri gösterecek iç içe geçmiş widget'lar oluştur
+        self.scrollWidget = QWidget()
+        self.iletisimBilgisiLayout = QVBoxLayout(self.scrollWidget)  # Linkler için QVBoxLayout
+
+        self.scrollArea.setWidget(self.scrollWidget)  # ScrollArea'ya widget ekle
+        # Arayüze elemanları ekle
+        self.layout.addWidget(self.ad_label)
+        self.layout.addWidget(self.ad_input)
+        self.layout.addWidget(self.katkida_bulunma_orani_label)
+        self.layout.addWidget(self.katkida_bulunma_orani)
+        self.layout.addWidget(self.iletisimBilgisiEkleBtn)
+        self.layout.addWidget(self.iletisimBilgisiLabel)
+        self.layout.addWidget(self.scrollArea)  # Ana layout'a ScrollArea ekle
+
+        self.setLayout(self.layout)
+    def iletisimBilgisiEkle(self, baslik="", link=""):
+        self.iletisim_bilgisi_sayisi += 1
+        text_layout = QHBoxLayout()
+
+        # Başlık için QLabel ve QLineEdit oluştur
+        label_baslik = QLabel("Başlık")
+        iletisim_bilgisi_baslik = QLineEdit(baslik)
+        text_layout.addWidget(label_baslik)
+        text_layout.addWidget(iletisim_bilgisi_baslik)
+
+        # Link için QLabel ve QLineEdit oluştur
+        label_link = QLabel("Link")
+        iletisim_bilgisi_link = QLineEdit(link)
+        text_layout.addWidget(label_link)
+        text_layout.addWidget(iletisim_bilgisi_link)
+
+        # Sil butonu ekle
+        sil_butonu = QPushButton("Sil")
+        sil_butonu.setStyleSheet(SIL_BUTONU_STILI)
+        sil_butonu.clicked.connect(lambda: self.silSatir(text_layout))
+        text_layout.addWidget(sil_butonu)
+
+        # Layout'u ana QVBoxLayout'e ekle
+        self.iletisimBilgisiLayout.addLayout(text_layout)
+        self.iletisimBilgisiLabel.setText("Toplam {} iletişim bilgisi".format(self.iletisim_bilgisi_sayisi))
+        if baslik == "" and link == "":
+            QMessageBox.information(self, "Bilgi", "İletişim bilgisi başarıyla eklendi! Göremiyorsanız aşağıya doğru kaydırın.")
+    def silSatir(self, layout):
+        self.iletisim_bilgisi_sayisi -= 1
+        # Belirtilen layout içindeki widget'ları temizle
+        while layout.count():
+            item = layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()
+        # Layout'u ana QVBoxLayout'den çıkar ve temizle
+        self.iletisimBilgisiLayout.removeItem(layout)
+        layout.deleteLater()
+        self.iletisimBilgisiLabel.setText("Toplam {} iletişim bilgisi".format(self.iletisim_bilgisi_sayisi))
+    def kaydet(self):
+        ad = self.ad_input.text()
+        if ad == "":
+            QMessageBox.warning(self, "Hata", "Ad boş bırakılamaz!")
+            return
+        if not self.iletisimBilgileriYukle():
+            return
+        emin_mi = QMessageBox.question(
+            self,
+            "Onay",
+            "Değişiklikleri kaydetmek istediğinden emin misin?",
+            QMessageBox.Yes | QMessageBox.No,
+        )
+
+        if emin_mi == QMessageBox.Yes:
+            self.thread = KatkiEkleThread(
+                ad,
+                self.kisi,
+                self.iletisim_bilgileri_dizisi,
+                KATKIDA_BULUNANLAR_JSON_PATH,
+                self.katkida_bulunma_orani.currentText(),
+                self.data,
+                self,
+            )
+            self.thread.finished.connect(self.islemSonucu)
+            # ProgressDialog'u göster
+            self.progressDialog.show()
+            self.thread.start()
+    def iletisimBilgileriYukle(self):
+        self.iletisim_bilgileri_dizisi[ILETISIM_BILGILERI] = []
+        for i in range(self.iletisimBilgisiLayout.count()):
+            # QVBoxLayout içindeki her bir QHBoxLayout'a eriş
+            hbox_layout = self.iletisimBilgisiLayout.itemAt(i).layout()
+
+            if hbox_layout is not None:
+                iletisim_bilgisi_baslik = hbox_layout.itemAt(1).widget().text()
+                iletisim_bilgisi_link = hbox_layout.itemAt(3).widget().text()
+                if iletisim_bilgisi_baslik == "" or iletisim_bilgisi_link == "":
+                    QMessageBox.warning(
+                        self, "Hata", "İletişim bilgileri boş bırakılamaz!"
+                    )
+                    return False
+                self.iletisim_bilgileri_dizisi[ILETISIM_BILGILERI].append(
+                    {BASLIK: iletisim_bilgisi_baslik, LINK: iletisim_bilgisi_link}
+                )
+        return True
+class KatkidaBulunanEkleWindow(BaseKatkidaBulunanWindow):
+    def __init__(self, data ,parent):
+        self.title = "Katkıda Bulunan Ekle"
+        super().__init__(parent)
+        self.parent = parent
+        self.kisi = None
+        self.data = data
+
+    def initUI(self):
+        super().initUI()
+        self.setWindowTitle(self.title)
         self.ekle_btn = QPushButton("Ekle", self)
         self.ekle_btn.setStyleSheet(EKLE_BUTONU_STILI)
-        # Butona basıldığında ekleme işlevini çalıştır
         self.ekle_btn.clicked.connect(self.kaydet)
-
-        # Arayüze elemanları ekle
-        layout.addWidget(self.name_label)
-        layout.addWidget(self.name_input)
-        layout.addWidget(self.github_label)
-        layout.addWidget(self.github_input)
-        layout.addWidget(self.katkida_bulunma_orani_label)
-        layout.addWidget(self.katkida_bulunma_orani)
-        layout.addWidget(self.ekle_btn)
-
-        self.setLayout(layout)
+        self.layout.addWidget(self.ekle_btn)
         self.show()
 
     def closeEvent(self, event):
         closeEventHandler(self, event, self.is_programmatic_close)
-
-
-    def kaydet(self):
-        # Thread örneğini oluştur
-        ad = self.name_input.text()
-        github_kullanici_adi = self.github_input.text()
-        self.thread = KatkiEkleThread(
-            ad, github_kullanici_adi, KATKIDA_BULUNANLAR_JSON_PATH, self.katkida_bulunma_orani.currentText(),self
-        )
-        self.thread.finished.connect(self.islemSonucu)
-        # ProgressDialog'u göster
-        self.progressDialog.show()
-        self.thread.start()
 
     def islemSonucu(self, success, message):
         self.progressDialog.close()
