2b9924fbd616e891d61da3afc9dc2a591ff37cdf
Muhammed Kayra Bulut <98055130+kaayra2000@users.noreply.github.com>
2024-03-13 14:12:31 +0300

    arayüzün puhs kısmına splitter eklendi

--- readme_olustur/readme_guncelleme_arayuzu_python/git_helper.py
+++ readme_olustur/readme_guncelleme_arayuzu_python/git_helper.py
@@ -13,7 +13,9 @@ from PyQt6.QtWidgets import (
     QSizePolicy,
     QHBoxLayout,
     QListWidgetItem,
+    QSplitter,
 )
+from PyQt6.QtCore import Qt
 import textwrap
 from progress_dialog import CustomProgressDialogWithCancel
 from threadler import CMDScriptRunnerThread
@@ -68,6 +70,10 @@ class GitHelper:
         subprocess.run(["git", "-C", repo_path, "push"])
 
     @staticmethod
+    def git_restore(repo_path, file_path):
+        subprocess.run(["git", "-C", repo_path, "restore", "file_path"])
+
+    @staticmethod
     def git_diff(repo_path, file_name):
         # Git diff komutunu çalıştır
         result = subprocess.run(
@@ -75,6 +81,14 @@ class GitHelper:
         )
         diff_output = result.stdout
         return diff_output
+
+
+def getMaxSizeYatay():
+    return QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+
+
+def getMaxSizeDikey():
+    return QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Expanding)
 
 
 class GitDialog(QDialog):
@@ -88,7 +102,8 @@ class GitDialog(QDialog):
     def initUI(self):
         self.setWindowTitle("Git Değişiklikleri")
         self.setGeometry(100, 100, 600, 400)
-
+        # Kaydedilmeyen ve Kaydedilen Değişiklikler için QSplitter oluştur
+        changesSplitter = QSplitter(Qt.Orientation.Vertical)
         layout = QVBoxLayout()
         staged_layout = QHBoxLayout()
         unstaged_layout = QHBoxLayout()
@@ -101,28 +116,31 @@ class GitDialog(QDialog):
         add_all_btn = QPushButton("Tüm değişiklikleri ekle")
         add_all_btn.setStyleSheet(EKLE_BUTONU_STILI)
         unstaged_label = QLabel("Kaydedilmeyen Değişiklikler")
-        unstaged_label.setSizePolicy(
-            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred
-        )
+        unstaged_label.setSizePolicy(getMaxSizeYatay())
         unstaged_layout.addWidget(unstaged_label)
         unstaged_layout.addWidget(add_all_btn)
-        layout.addLayout(unstaged_layout)
-        layout.addWidget(self.unstagedList)
+        unstaged_widget = QWidget()
+        unstaged_widget.setLayout(unstaged_layout)
+        changesSplitter.addWidget(unstaged_widget)
+        changesSplitter.addWidget(self.unstagedList)
         staged_label = QLabel("Kaydedilen Değişiklikler")
-        staged_label.setSizePolicy(
-            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred
-        )
+        staged_label.setSizePolicy(getMaxSizeYatay())
         delete_all_btn = QPushButton("Tüm değişiklikleri sil")
         delete_all_btn.setStyleSheet(SIL_BUTONU_STILI)
         staged_layout.addWidget(staged_label)
         staged_layout.addWidget(delete_all_btn)
-        layout.addLayout(staged_layout)
-        layout.addWidget(self.stagedList)
+        staged_widget = QWidget()
+        staged_widget.setLayout(staged_layout)
+        changesSplitter.addWidget(staged_widget)
+        changesSplitter.addWidget(self.stagedList)
+        layout.addWidget(changesSplitter)
         layout.addWidget(QLabel("Commit Mesajı"))
         layout.addWidget(self.commitMessage)
         layout.addWidget(self.amendCheckBox)
         layout.addWidget(pushButton)
         self.setLayout(layout)
+        self.stagedList.setSizePolicy(getMaxSizeDikey())
+        self.unstagedList.setSizePolicy(getMaxSizeDikey())
         add_all_btn.clicked.connect(self.tumDegisiklikleriEkle)
         pushButton.clicked.connect(self.pushChanges)
         delete_all_btn.clicked.connect(self.tumDegisiklikleriSil)
@@ -234,7 +252,6 @@ class GitDialog(QDialog):
             return
         commit_msg = '"' + commit_msg + '"'
         self.commit_and_push(commit_msg=commit_msg)
-        self.close()
 
     def commit_and_push(self, commit_msg):
         self.progress_dialog = CustomProgressDialogWithCancel(
@@ -268,6 +285,7 @@ class GitDialog(QDialog):
         self.thread.wait()
         del self.thread
         del self.progress_dialog
+        self.close()
         QMessageBox.information(self, "Başarılı", output)
 
     def thread_durduruluyor(self):
@@ -296,19 +314,28 @@ class FileItemWidget(QWidget):
         layout = QHBoxLayout()
         btn_layout = QVBoxLayout()
         self.label = QLabel()
-        self.label.setSizePolicy(
-            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred
-        )
-
+        self.label.setSizePolicy(getMaxSizeYatay())
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek
+        # burası güncellenecek restore buton eklenecek GitHelper.git_restore
         self.label.setText(self.elideText(self.file_name))
         self.label.setToolTip(self.file_name)  # Tam dosya adını tooltip olarak ekleme
         button = QPushButton("Kaydet" if not self.is_staged else "Sil")
         button.setStyleSheet(SIL_BUTONU_STILI if self.is_staged else EKLE_BUTONU_STILI)
         button.clicked.connect(self.onButtonClick)
-
         diff_btn = QPushButton("Farkı Gör")
         diff_btn.clicked.connect(self.showGitDiff)
-
         btn_layout.addWidget(button)
         btn_layout.addWidget(diff_btn)
         layout.addWidget(self.label)
@@ -340,9 +367,43 @@ class FileItemWidget(QWidget):
 
     def showGitDiff(self):
         diff_output = GitHelper.git_diff(self.parent.repo_path, self.file_name)
+        diffWindow = DiffWindow(diff_output, diff_output, self)
+        diffWindow.show()
 
-        # Diff çıktısını bir QMessageBox'ta göster
-        msgBox = QMessageBox()
-        msgBox.setWindowTitle("Değişiklikler")
-        msgBox.setText(diff_output)
-        msgBox.exec()
+
+class DiffWindow(QDialog):
+    # DİFF GÖSTERİLMİYOR ŞU AN DÜZENLENECEK
+    def __init__(self, old_text, new_text, parent=None):
+        super().__init__(parent=parent)
+        self.setModal(True)
+        self.setMinimumSize(400, 400)
+        self.old_text = old_text
+        self.new_text = new_text
+        self.initUI()
+
+    def initUI(self):
+        # Layoutları tanımla
+        self.layout = QHBoxLayout()
+        self.eski_layout = QVBoxLayout()
+        self.yeni_layout = QVBoxLayout()
+
+        self.eski_layout.addWidget(QLabel("Eskisi"))
+        # Eski metni göster
+        self.oldTextEdit = QTextEdit()
+        self.oldTextEdit.setPlainText(self.old_text)
+        self.oldTextEdit.setReadOnly(True)
+
+        self.eski_layout.addWidget(self.oldTextEdit)
+        self.yeni_layout.addWidget(QLabel("Yenisi"))
+        # Yeni metni göster
+        self.newTextEdit = QTextEdit()
+        self.newTextEdit.setPlainText(self.new_text)
+        self.newTextEdit.setReadOnly(True)
+        self.yeni_layout.addWidget(self.newTextEdit)
+        # Layoutlara widget'ları ekle
+        self.layout.addLayout(self.eski_layout)
+        self.layout.addLayout(self.yeni_layout)
+
+        # Ana pencerenin layout'unu ayarla
+        self.setLayout(self.layout)
+        self.setWindowTitle("Fark Gösterici")
