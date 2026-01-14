a94a5038293839f6205dc201379bcc6ce7145882
Muhammed Kayra Bulut <kayrabulut39@gmail.com>
2026-01-14 07:40:05 +0300

    şevval çabuk burak başol nesne labları klasör farkı giderildi

--- 2-1/Nesneye Yonelik Programlama/kodlar_lablar/kodlar/2025/uygulamalar/uyg4/WriteMain.java
+++
@@ -1,8 +0,0 @@
-package Uyg4;
-
-public class WriteMain {
-    public static void main(String[] args) throws Exception {
-        new Bank("Temp").createDataFile("data.txt");
-        System.out.println("Dosya oluşturuldu (tek thread sonucu = +10)");
-    }
-}
